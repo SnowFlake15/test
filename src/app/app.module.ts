883be0617c6da5e9023be7c5721d8a5ee2743f7f
Sesili <sesili.lomadze.1@btu.edu.ge>
2022-08-08 11:42:49 +0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlablaComponent } from './blabla/blabla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlablaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/blabla/blabla.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BlablaComponent } from './blabla.component';
+
+describe('BlablaComponent', () => {
+  let component: BlablaComponent;
+  let fixture: ComponentFixture<BlablaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BlablaComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BlablaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
